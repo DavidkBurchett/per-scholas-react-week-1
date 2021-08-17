3980aec2b10d019d372d8fae2f9e59f266905770
David_Kyle_Burchett <davidkburchett@gmail.com>
2021-08-17 11:40:11 -0400

    created package.json and JS object examples

---
+++ js-object-examples.js
@@ -0,0 +1,17 @@
+const myInfo = {
+    "name": "Kyle",
+    "birthday": 11242001,
+    "citiesLivedIn": ["Charlotte"],
+    "isHuman": true,
+    "planetsVisited": null,
+    "personalInfo": {
+        "favoriteFood": "pizza"
+    }
+};
+
+console.log('My info printed as an object');
+console.log(myInfo);
+
+console.log('my info printed as a strinf in the JSON format');
+const myInfoAsJSON = JSON.stringify(myInfo);
+console.log(myInfoAsJSON);
